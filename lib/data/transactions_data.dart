e3e460af048d40ab35dd9e037070f2cef7c62fb3
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2023-08-11 13:55:28 -0300

    create: model and data

---
+++ lib/data/transactions_data.dart
@@ -0,0 +1,24 @@
+import 'package:personal_expenses/models/transaction.dart';
+
+class TransactionsData {
+  static final transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Tênis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Camisa',
+      value: 110.57,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Calça',
+      value: 200.13,
+      date: DateTime.now(),
+    ),
+  ];
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/data/transactions_data.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _MyHomePageState();
@@ -15,14 +14,31 @@ class _MyHomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Despesas pessoais'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              //width: MediaQuery.of(context).size.width,
+              child: Card(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                elevation: 5,
+                child: const Center(child: Text('Gráfico')),
+              ),
+            ),
+            Column(
+              children: [
+                ...TransactionsData.transactions.map(
+                  (t) {
+                    return Card(
+                      child: Text(t.title),
+                    );
+                  },
+                ).toList()
+              ],
             ),
           ],
         ),
